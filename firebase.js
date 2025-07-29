354bed6c180bb200d8c364b0f8d1779bf31f91ba
2524 <yotaoku0307@gmail.com>
2025-07-30 06:42:48 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// firebase.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyAweBQJ17LYvPsfYUCBLygwdehoTEOrsiQ",
+  authDomain: "project-7411251555753678403.firebaseapp.com",
+  databaseURL: "https://project-7411251555753678403-default-rtdb.firebaseio.com",
+  projectId: "project-7411251555753678403",
+  storageBucket: "project-7411251555753678403.appspot.com",
+  messagingSenderId: "172462885590",
+  appId: "1:172462885590:web:bf2ca6b6e7e668f1445777"
+};
